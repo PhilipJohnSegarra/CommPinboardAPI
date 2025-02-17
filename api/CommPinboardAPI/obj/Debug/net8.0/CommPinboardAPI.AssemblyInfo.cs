4bb8f1ab500a19d3ab80d73397d2a785d8726cd1
philip <philipjohn.segarra@gmail.com>
2025-02-17 15:26:40 +0800

    new database

--- api/CommPinboardAPI/obj/Debug/net8.0/CommPinboardAPI.AssemblyInfo.cs
+++ api/CommPinboardAPI/obj/Debug/net8.0/CommPinboardAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommPinboardAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e616b92fcaae5afc11a2a1724bc2547d3d4d3c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7379c429254f927a4832568eb8b2bf41f62c0f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommPinboardAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommPinboardAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
