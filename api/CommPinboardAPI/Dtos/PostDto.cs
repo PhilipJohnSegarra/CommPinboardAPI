7fbb807942f49e6fd017ed971f51d84548374540
philip <philipjohn.segarra@gmail.com>
2025-02-22 16:39:19 +0800

    Datetime fix

--- api/CommPinboardAPI/Dtos/PostDto.cs
+++ api/CommPinboardAPI/Dtos/PostDto.cs
@@ -22,9 +22,9 @@ namespace CommPinboardAPI.Dtos
 
         public UsersDto? User { get; set; }
 
-        public DateTime DateCreated { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
 
-        public DateTime DateUpdated { get; set;}
+        public DateTime DateUpdated { get; set;} = DateTime.Now;
 
     }
 }
